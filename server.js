a0bd5910d4aef55af8ce764ae266a07626c9e0d1
sharadbhat <sharad.mbhat@gmail.com>
2021-03-03 13:14:48 +0530

    port changes

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const path = require('path')
 
 const app = express()
 
+let port = process.env.PORT || 80
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'))
 })
 
-app.listen(8000, '', () => {
+app.listen(port, '', () => {
   console.log('Server started at port 8000')
 })
