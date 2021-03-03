237da85bc43ee0983f98d0aeaecac72d172838a8
sharadbhat <sharad.mbhat@gmail.com>
2021-03-03 13:35:03 +0530

    mime type

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+// const mime = require('mime-types')
 const path = require('path')
 
 const app = express()
@@ -6,6 +7,8 @@ const app = express()
 let port = process.env.PORT || 80
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+express.static.mime.define({'video/mp4': ['mp4']})
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'))
