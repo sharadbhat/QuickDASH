a9cc0951e999f9587bbdd0c9776adcc5922e28fd
sharadbhat <sharad.mbhat@gmail.com>
2021-03-04 02:01:27 +0530

    minor change

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app = express()
 
 let port = process.env.PORT || 80
 
-app.use(express.static(path.join(__dirname, 'public')))
+// express.static.mime.define({'video/mp4': ['mp4']})
+// app.use(express.static(path.join(__dirname, 'public')))
 
-express.static.mime.define({'video/mp4': ['mp4']})
+app.get('/public/:filename', function(req, res) {
+  res.sendFile(path.join(__dirname, req.params.filename));
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'))
