13ea3198a614eea519108288e6f297ef01c7249c
sharadbhat <sharad.mbhat@gmail.com>
2021-03-04 02:51:40 +0530

    reset

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 const express = require('express')
-// const mime = require('mime-types')
 const path = require('path')
 
 const app = express()
 
 let port = process.env.PORT || 80
 
-// express.static.mime.define({'video/mp4': ['mp4']})
-// app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/public/src/:filename', function(req, res) {
-  res.contentType('mp4')
-  res.sendFile(path.join(__dirname, 'public', 'src', req.params.filename));
-});
-
-app.get('/public/dest/:filename', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'dest', req.params.filename));
-});
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'))
